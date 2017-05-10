1f3a43f70be3ab3e4dd9925bc743a33c3c607cb4
Michael Antczak <Michael.Antczak@gmail.com>
2017-05-10 15:38:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const path = require('path');
 const XLSX = require('xlsx');
 
 
-// here we have all the helper functions for the script to work
+// here we have the helper functions for the script to work
 const AUX = require('./auxiliary');
 
 // files used in the script
@@ -13,11 +13,13 @@ var outputJSON = "output.json";
 var outputTXT = "output.txt";
 
 // Prepare the input file
+// https://www.npmjs.com/package/xlsx
 var inputWorkbook = XLSX.readFile(inputFileName);
 var first_sheet_name = inputWorkbook.SheetNames[0];
 var inputWorksheet = inputWorkbook.Sheets[first_sheet_name];
  
 // Prepare the searched file
+// https://www.npmjs.com/package/xlsx
 var searchedWorkbook = XLSX.readFile(searchedFileName);
 var searched_sheet_name = searchedWorkbook.SheetNames[0];
 var searchedWorksheet = searchedWorkbook.Sheets[searched_sheet_name];
@@ -26,8 +28,8 @@ var searchedWorksheet = searchedWorkbook.Sheets[searched_sheet_name];
 var notFound = [];
 var notFoundString = "";
 
-// OUTER LOOP
-/* Loop ISBN cells in input file */
+/*      OUTER LOOP
+        Loop ISBN cells in input file */
 for (var outer = 2; outer < 4389; outer++) {
 
     // Set the flag 
@@ -41,41 +43,35 @@ for (var outer = 2; outer < 4389; outer++) {
     var desired_C_cell = inputWorksheet['C' + outer];
     var searched_C_value = (desired_C_cell ? desired_C_cell.v : undefined);
 
-    // console.log("\nWe are looking for ", searched_B_value, " - OR - ", searched_C_value, " -- ", inputWorksheet['D' + outer].v, "\n");
-
-    // console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-    // console.log(" -- START SEARCHING -- ");
-
-
-                // // INNER LOOP - searches the searched File
-                // // search the searchedWorkbook with inner loop
+                // INNER LOOP - searches the searched File
                 for (var inner = 2; inner < 4360; inner++) {
 
+                    // we use replace function to normalise ISBNs
+                    // Example: changes 978-879-89-21-234 to 9788798921234 
                     if (searchedWorksheet['B' + inner].v.replace(/-/g, "").includes(searched_B_value) ) {
 
                         found = true;
-                        // console.log("Found one! Line number : ", inner);
-                        // console.log("ISBN : ", searchedWorksheet['B' + inner].v, " - ", searchedWorksheet['A' + inner].v)
 
                     } 
 
+                    // we use replace function to normalise ISBNs
+                    // Example: changes 978-879-89-21-234 to 9788798921234 
                     if (searchedWorksheet['B' + inner].v.replace(/-/g, "").includes(searched_C_value) ) {
 
                         found = true;
-                        // console.log("Found one! Line number : ", inner);
-                        // console.log("ISBN : ", searchedWorksheet['B' + inner].v, " - ", searchedWorksheet['A' + inner].v)
 
                     } 
 
-                } // end of inner loop no. 2
+                } // end of inner loop
 
-
-    // console.log(" -- END OF SEARCHING -- \n\n");
-    
+                
     if (found === true) {
-        // reset flag
+
+        // reset flag, the item was found
         found = false;
+    
     } else {
+
         // item not found, so push to notFound Array
         var temp = {}
         temp.line = outer; 
@@ -84,13 +80,14 @@ for (var outer = 2; outer < 4389; outer++) {
 
         notFound.push(temp);
 
+        // and also append the notFoundString
         notFoundString += outer + "," + searched_B_value + "," + inputWorksheet['D' + outer].v + "\n";
 
     }
 
-}
+}   // END OUTER LOOP
 
-// // Out of the loops so print not Found items
-console.log("Number of items : ", notFound.length)
+// Out of the both loops so print not found items
+console.log("Number of items : ", notFound.length);
 AUX.writeOutput(outputJSON, JSON.stringify(notFound));
 AUX.writeOutput(outputTXT, notFoundString);
